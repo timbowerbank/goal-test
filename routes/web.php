5a3aabb150267de12dac04ed1b8b105a8b5b378d
Tim Bowerbank <tim@pendigital.co.uk>
2026-03-04 10:29:28 +0000

    Added in the client dashboard controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Client\ClientDashboardController;
+
+
 
 Route::get('/', function () {
     return view('carer.dashboard');
 });
+
+Route::get('/dashboard',[ClientDashboardController::class, 'index']);
