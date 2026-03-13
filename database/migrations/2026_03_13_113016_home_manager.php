351f503185c00a379802c3b2747b28235c574a52
Tim Bowerbank <tim@pendigital.co.uk>
2026-03-13 11:54:25 +0000

    Added pivot table for home_manager and also amend home_organisation

---
+++ database/migrations/2026_03_13_113016_home_manager.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('home_manager', function(Blueprint $table){
+            $table->foreignId('user_id')->constrained('users', 'id')->onDelete('restrict');
+            $table->foreignUlid('home_id')->constrained('homes', 'id')->onDelete('restrict');
+            $table->primary(['user_id', 'home_id']);
+            $table->timestamp('started_at')->index();
+            $table->timestamp('ended_at')->nullable();
+            $table->foreignId('created_by_user_id')->constrained('users', 'id')->onDelete('restrict');
+            $table->foreignId('updated_by_user_id')->nullable()->constrained('users', 'id')->onDelete('restrict');
+            $table->index('home_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('home_manager');
+    }
+};
