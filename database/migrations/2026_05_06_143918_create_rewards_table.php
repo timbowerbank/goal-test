3d80ad8fe38a6ffc229fc7790241dddaf403e54f
Tim Bowerbank <tim@pendigital.co.uk>
2026-05-06 15:58:19 +0100

    Added in migration for goals-> rewards

---
+++ database/migrations/2026_05_06_143918_create_rewards_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rewards', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->string('title', length: 40);
+            $table->string('description', length: 200);
+            $table->string('image_url', length: 200)->nullable();
+            $table->foreignId('updated_by_user_id')->nullable()->constrained('users', 'id')->onDelete('restrict');            
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rewards');
+    }
+};
