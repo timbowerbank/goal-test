d3d61ff6ec4bf916d10a24ba8890e23032df63a5
Tim Bowerbank <tim@pendigital.co.uk>
2026-05-06 09:52:25 +0100

    Added carers list for home dashboard

--- resources/views/components/shared/list-carers.blade.php
+++ resources/views/components/shared/list-carers.blade.php
@@ -1,23 +1,23 @@
 @props([
-    'clients',
+    'carers',
     'home'
 ])
+
 <div class="p-4 rounded border mb-2 pd-list-card-with-scroll">
-    <h2>Clients at {{ $home->home_name }}</h2>
-    @if($clients !== null)
+    <h2>Carers at {{ $home->home_name }}</h2>
+    @if($carers !== null)
     <div class="pd-table-scroll-outer">
         <table class="w-100 table table-striped">
-
             <thead>
                 <tr>
-                    <th scope="col">Client Name</th>
+                    <th scope="col">Carer Name</th>
                     <th>View</th>
                 </tr>
             </thead>
             <tbody>
-                @foreach($clients as $client)
+                @foreach($carers as $carer)
                 <tr>
-                    <td scope="row">{{ $client->user->full_name }}</td>
+                    <td scope="row">{{ $carer->user->full_name }}</td>
                     <td>
                         <a href="#" class="btn btn-secondary btn-sm">View</a>
                     </td>
@@ -29,7 +29,7 @@
     </div>
 
     @else
-    <p>Sorry, there aren't any clients at this home.</p>
+    <p>Sorry, there aren't any carers assigned to this home.</p>
 
     @endif
 </div>
--- resources/views/manager/home.blade.php
+++ resources/views/manager/home.blade.php
@@ -3,11 +3,14 @@
         @section('title', $home->home_name)
 
         @section('manager-content')
-        <x-shared.header headline="{{ $home->home_name }}" sub-headline="See clients and carers for {{ $home->home_name }}"></x-shared.header>
+        <x-shared.header headline="{{ $home->home_name }}" sub-headline="Welcome to {{ $home->home_name }}"></x-shared.header>
 
         <div class="row">
             <div class="col-md-6">
                 <x-shared.list-clients :clients="$home->clients" :home="$home"></x-shared.list-clients>
+            </div>
+            <div class="col-md-6">
+                <x-shared.list-carers :carers="$home->carers" :home="$home"></x-shared.list-carers>
             </div>
         </div>
 
