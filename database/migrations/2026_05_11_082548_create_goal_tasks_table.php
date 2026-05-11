2a54ce9951f969471c10a19b70407c70356ef649
Tim Bowerbank <tim@pendigital.co.uk>
2026-05-11 09:43:07 +0100

    Added in goal_tasks migration

--- database/migrations/2026_05_11_082548_create_goal_tasks_table.php
+++ database/migrations/2026_05_11_082548_create_goal_tasks_table.php
@@ -12,8 +12,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('goal_tasks', function (Blueprint $table) {
-            $table->id();
+            $table->ulid('id')->primary();
+            $table->foreignUlid('goal_id')->constrained('goals')->onDelete('restrict');
+            $table->string('title', length:40);
+            $table->text('description');
+            $table->string('priority', length:20)->index();
+            $table->foreignId('assigned_to_user_id')->nullable()->constrained('users')->onDelete('restrict');
+            $table->timestamp('due_at')->index();
+            $table->timestamp('completed_at')->nullable()->index();
+            $table->foreignId('completed_with_user_id')->nullable()->constrained('users')->onDelete('restrict');
+            $table->timestamp('archived_at')->nullable();
+            $table->foreignId('archived_by_user_id')->nullable()->constrained('users')->onDelete('restrict');
+            $table->string('goal_task_status', length:20)->default('not started')->index();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
