d5d1fe8a2cac7d0bc88b4afe8e43250508166491
Tim Bowerbank <tim@pendigital.co.uk>
2026-06-10 13:47:43 +0100

    Added in simoplified appraoch for drops downs in select task component

--- resources/views/components/task/select-bar.blade.php
+++ resources/views/components/task/select-bar.blade.php
@@ -21,48 +21,21 @@
                     @csrf
                     <div class="d-flex flex-column flex-md-row align-items-center ">
                         <select name="filterType" class="form-select mb-2 mb-md-0" aria-label="Default select example">
+                            @php
+                                $filterLabels = ['all' => 'All tasks', 'due' => 'Due this week', 'overdue' => 'Overdue tasks'];
+                            @endphp
                             @foreach($filterTypes as $filter)
-                                @if($filterSelected === $filter)
-                                    @if($filter === 'due')
-                                    <option selected value="{{ $filter }}">Due this week</option>
-                                    @elseif($filter === 'overdue')
-                                    <option selected value="{{ $filter }}">Overdue tasks</option>
-                                    @else
-                                    <option selected value="{{ $filter }}">All tasks</option>
-                                    @endif
-                                @else
-                                    @if($filter === 'due')
-                                    <option value="{{ $filter }}">Due this week</option>
-                                    @elseif($filter === 'overdue')
-                                    <option value="{{ $filter }}">Overdue tasks</option>
-                                    @else
-                                    <option value="{{ $filter }}">All tasks</option>
-                                    @endif
-                                @endif
+                                <option value="{{ $filter }}" {{ $filterSelected === $filter ? 'selected' : ''}}>{{ $filterLabels[$filter] }}</option>
                             @endforeach
                         </select>
 
                         <span class="ms-2 me-2 mb-2 mb-md-0">+</span>
 
                         <select name="client" class="form-select mb-4 mb-md-0" aria-label="Default select example">
-                            @if($clientSelectedId === 'all')
-                                <option selected value="all">All Clients</option>
-                                @foreach($allHomeClients as $client)
-                                    <option value="{{ $client->id }}">{{ $client->user->full_name }}</option>
-                                @endforeach
-                            @else 
-                                @foreach($allHomeClients as $client)
-                                    @if($client->id === $clientSelectedId)
-                                        <option selected value="{{ $client->id }}">{{ $client->user->full_name }}</option>
-                                        @continue
-                                    @else
-                                        <option value="{{ $client->id }}">{{ $client->user->full_name }}</option>
-                                    @endif
-                                        
-                                @endforeach
-                                <option value="all">All Clients</option>
-                            @endif
-
+                            <option value="all" {{ $clientSelectedId === 'all' ? 'selected' : '' }}>All clients</option>
+                            @foreach ($allHomeClients as $client)
+                                <option value="{{ $client->id }}" {{ $clientSelectedId === $client->id ? 'selected' : '' }}>{{ $client->user->full_name }}</option>
+                            @endforeach
                         </select>
                         <button type="submit" class="ms-3 btn btn-primary">Go</button>
 
@@ -70,38 +43,14 @@
                 </form>
 
             </div>
-
-
-
-
         </div>
-
     </div>
     <div class="row">
-        <div class="col"><p class="mb-0">Showing 
+        @php
+            $filterLabel = $filterLabels[$filterSelected];
+            $clientLabel = $clientSelectedId === 'all' ? 'all clients' : $allHomeClients->firstWhere('id', $clientSelectedId)?->user->full_name;
 
-            @if($filterSelected === 'all')
-            <strong>all tasks</strong>
-            @elseif($filterSelected === 'due')
-            <strong>tasks due this week </strong>
-            @else
-            <strong>overdue tasks</strong>
-            @endif
-            
-
-            for 
-            @if($clientSelectedId === 'all')
-            <strong>all clients</strong> 
-            @else
-            <strong>
-                @foreach($allHomeClients as $client)
-                    @if($client->id === $clientSelectedId)
-                        {{ $client->user->full_name }}
-                        @break
-                    @endif
-                @endforeach
-            </strong>
-            @endif
-            at <strong>{{ $homeName }}</strong></p></div>
+        @endphp
+        <div class="col"><p class="mb-0">Showing <strong>{{ strtolower($filterLabel) }}</strong> for <strong>{{ $clientLabel }}</strong> at <strong>{{ $homeName }}</strong></p></div>
     </div>
 </div>
